75c076a769d75464f25326b318952f9485294e5f
Chris Pryer <cnpryer@gmail.com>
2023-05-04 18:02:32 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+// This is an example program used to demonstrate encryption and decryption in Rust.
+
+use std::num::NonZeroU32;
+
+use ring::{aead::*, rand::{SecureRandom, SystemRandom}, pbkdf2};
+use human_panic::setup_panic;
+
+
+fn main() {
+    setup_panic!();
+
+    let passphrase = "my secret passphrase";
+    let mut data = b"hello, this is my secret message".to_vec();
+    let original_data = data.clone();
+
+    let salt = [0; 16];
+    let key = derive_key_from_passphrase(passphrase, &salt);
+    let mut nonce_data = [0; 12];
+    SystemRandom::new().fill(&mut nonce_data).unwrap();
+
+    let key = UnboundKey::new(&CHACHA20_POLY1305, &key).unwrap();
+    let key = LessSafeKey::new(key);
+
+    let nonce = Nonce::assume_unique_for_key(nonce_data);
+    key.seal_in_place_append_tag(nonce, Aad::empty(), &mut data)
+        .unwrap();
+
+    assert_ne!(original_data, data);
+
+    let nonce = Nonce::assume_unique_for_key(nonce_data);
+    let data = key.open_in_place(nonce, Aad::empty(), &mut data).unwrap();
+
+    assert_eq!(original_data, data);
+}
+
+fn derive_key_from_passphrase(passphrase: &str, salt: &[u8]) -> [u8; 32] {
+    let mut key = [0u8; 32];
+    let iterations = 100_000;
+
+    pbkdf2::derive(
+        pbkdf2::PBKDF2_HMAC_SHA256,
+        NonZeroU32::new(iterations).unwrap(),
+        salt,
+        passphrase.as_bytes(),
+        &mut key,
+    );
+
+    key
+}
